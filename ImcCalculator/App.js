49ab37b389d3de5522c13b23dc246a26b3c5aaca
Davi ferreira de sena <davifsena2@gmail.com>
2023-08-13 10:09:17 -0300

    add Title

--- ImcCalculator/App.js
+++ ImcCalculator/App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from "./src/components/Title";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Imc Calculator</Text>
-      <StatusBar style="auto" />
+      <Title/>
     </View>
   );
 }
